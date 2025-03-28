2a3d1e26c8d4971eec7ed0d5932dc9285c342d35
Student <509-2@intc.local>
2025-03-28 10:02:24 +0500

    Добавлен код и настройка CI

--- task1.py
+++ task1.py
@@ -6,3 +6,13 @@ def calculate_area(shape, **params):
         return pi * (params["radius"] ** 2)
     else:
         raise ValueError("Unsupported shape")
+
+#
+#
+#git remote add origin https://github.com/твой-юзернейм/твой-репозиторий.git
+#git branch -M main
+#git push -u origin main
+
+#git add geometry.py
+#git commit -m "Добавлена функция calculate_area"
+#git push origin main
